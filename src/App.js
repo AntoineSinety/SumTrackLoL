827384495ea9525e69d6bd0586f30e5310f7e934
Antoine de Sinety <antoine.desinety@gmail.com>
2022-03-17 11:40:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import './assets/css/style.css'
+import Switch from "react-switch";
+
+import React, { useState } from "react";
+import { getInfoSummoner, getCurrentGame } from './api/riot';
 
 function App() {
+
+  const [name, setName] = useState("");
+
+  const [participants, setParticipants] = useState(null);
+
+  const [bottes, setBottes] = useState(false);
+  
+  const handleSubmit = async (evt) => {
+      evt.preventDefault();
+
+      const infoSumm = await getInfoSummoner(name)
+
+      console.log('inf', infoSumm);
+      const currentGame = await getCurrentGame(infoSumm.id)
+      console.log('current game', currentGame)
+
+      setParticipants(currentGame.participants);
+
+
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nom d'invocateur :
+            <input type="text" name="name"  value={name} onChange={e => setName(e.target.value)}/>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <div>
+          <h2>Liste des joueurs :</h2>
+          <div className="container-summoner">
+
+            { participants &&
+              participants.map((player) => 
+              <div className={'wrapper-summoner ' + (player.teamId === 100 ? "blue" : "red")}>
+                  <p>{player.summonerName}</p>
+                  <p>Runes Cosmic insight : <br/>
+                  {
+                    player.perks.perkIds.includes(8347) ? "oui" : "non"
+                  }
+                  </p>
+                <div>Bottes de lucidité:<br/>
+                <Switch onChange={(bottes) => setBottes(bottes)} checked={bottes} />
+                </div>
+              </div>
+              )
+            }
+          </div>
+         
+        </div>
       </header>
     </div>
   );
---
+++ src/api/riot.js
@@ -0,0 +1,32 @@
+const key = "RGAPI-c82c74d6-0810-4d79-bf25-d8d2d38f3e4e";
+
+
+export const getInfoSummoner = async (name) =>{
+	var getPuuidEUWAcc = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+name +'?api_key='+key;
+
+	return fetch(getPuuidEUWAcc)
+	.then(response => response.json())
+	.then(data => {
+        return data
+    })
+}
+
+export const getCurrentGame = async (name) =>{
+	var getPuuidEUWAcc = 'https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/'+name +'?api_key='+key;
+
+	return fetch(getPuuidEUWAcc)
+	.then(response => response.json())
+	.then(data => {
+        return data
+    })
+}
+
+// export const getAllRunes = async (name) =>{
+// 	var getPuuidEUWAcc = 'http://ddragon.leagueoflegends.com/cdn/12.5.1/data/en_US/runesReforged.json';
+
+// 	return fetch(getPuuidEUWAcc)
+// 	.then(response => response.json())
+// 	.then(data => {
+//         return data
+//     })
+// }
